0a3245703309711887b3c7d07e486f16a947f158
Tiago Oliveira Dallecio <111533884+TERRIBLETOD@users.noreply.github.com>
2022-08-23 14:24:33 -0300

    Add files via upload
    Função de achar o maior e menor valor do vetor

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 int* aloc(){
-    int *vet,i=0;
+    int *vet;
     if((vet=(int*) calloc(1000,sizeof(int)))==NULL) exit(1);
     return vet;
 }
@@ -12,16 +13,37 @@ void preenche(int* vet){
     for(i=0;i<1000;i++){
         printf("\n");
         for(k=0;k<20;k++){
-        vet[i]=rand()%100000;printf("%d\t",vet[i]);
+        vet[i]=rand()%100000;
+        printf("%d\t",vet[i]);
         i++;
     }}
 
 }
-int main()
+
+void acha_menor_maior(int* vet)
+{
+int i=0;
+int*menor,*maior;
+
+ menor = vet+i;
+ maior = vet+i;
+
+ for(i=0;i<1000;i++)
+ {
+     if(*menor>*(vet+i)&& *(vet+i)!= 0)
+         menor = (vet+i);
+     if(*maior<*(vet+i))
+        maior = (vet+i);
+ }
+    printf("\n\nO maior numero do vetor eh: %d",*maior);
+    printf("\nO menor numero do vetor eh: %d\n\n",*menor);
+}
+ int main()
 {
     int *vet;
     vet=aloc();
     preenche(vet);
+    acha_menor_maior(vet);
 
     return 0;
 }
