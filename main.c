28e8554d45a59887007b65a66b4fe6d94a8fb62f
Renan Rohers Slavador <111884210+r0HERS@users.noreply.github.com>
2022-08-26 08:47:09 -0300

    Aplicação do bubble sort, merge main.

--- main.c
+++ main.c
@@ -7,18 +7,34 @@ int* aloc(){
     if((vet=(int*) calloc(1000,sizeof(int)))==NULL) exit(1);
     return vet;
 }
+void bubble_sort(int* vet) {
+    int i = 0, j = 0,l = 0,k=0, tmp;
+    for (i = 0; i < 1000; i++) {
+        for (j = 0; j < 1000 - i - 1; j++) {
+            if (vet[j] > vet[j + 1]) {
+                tmp = vet[j];
+                vet[j] = vet[j + 1];
+                vet[j + 1] = tmp;
+            }
+        }
+    }
+    for(l=0;l<1000;l++){
+        printf("\n");
+    for(k=0;k<10;k++){
+        printf("    %d",vet[l]);
+        l++;
+        }
+    }
+}
 void preenche(int* vet){
     srand((unsigned)time(NULL));
-    int i=0,k=0;
+    int i=0;
     for(i=0;i<1000;i++){
-        printf("\n");
-        for(k=0;k<10;k++){
+
         vet[i]=rand()%100000;
-        printf("%d\t",vet[i]);
-        i++;
     }}
 
-}
+
 void achamediana(int *vet){
     int i=0;
     float mediana;
@@ -61,5 +77,6 @@ int main()
     acha_menor_maior(vet);
     tirarmedia(vet);
     achamediana(vet);
+    bubble_sort(vet);
     return 0;
 }
